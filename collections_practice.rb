3940cbe1dad0d1e9c472b90f43924a25ccb8278b
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-05 01:04:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+   b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+   a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
